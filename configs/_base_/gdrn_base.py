4c1902dfbdbf93776333def9aca24ee491c525a2
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-08-14 14:08:14 -0400

    Change np.bool for bool
    Make code compatible with numpy version 1.20 and above.

--- configs/_base_/gdrn_base.py
+++ configs/_base_/gdrn_base.py
@@ -117,7 +117,7 @@ MODEL = dict(
             MASK_LOSS_TYPE="L1",  # L1 | BCE | CE | RW_BCE | dice
             MASK_LOSS_GT="trunc",  # trunc | visib | gt
             MASK_LW=1.0,
-            # region loss -------------------------
+            # Region loss -------------------------
             REGION_LOSS_TYPE="CE",  # CE
             REGION_LOSS_MASK_GT="visib",  # trunc | visib | obj
             REGION_LW=1.0,
