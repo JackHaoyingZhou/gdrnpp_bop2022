fab1006b45b9641dc9fcc4dc74dd93ec2f586107
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-08-21 14:45:20 -0400

    Add simple dataconfig class

---
+++ scripts/gdrn_simple/DatasetConfig.py
@@ -0,0 +1,41 @@
+from lib.pysixd.config import datasets_path
+import os.path as osp
+
+def get_dataset_config(name:str):
+    dataset_dicts ={
+        "ambf_suturing": DatasetConfig("ambf_suturing", "ambf_suturing", {1: "needle"}),
+        "tudl_bop_test": DatasetConfig("tudl_bop_test", "tudl", {1: "dragon", 2: "frog", 3: "can"}),
+    } 
+    return dataset_dicts[name]
+
+class DatasetConfig:
+    def __init__(self, name: str, data_root:str, id2obj: dict):
+        self._NAME = name
+        self._ID2OBJ = id2obj 
+        self._DATA_ROOT = data_root
+        self._MODEL_PATHS = [osp.join(datasets_path, self._DATA_ROOT, "models",
+                                       f"obj_{obj_id:06d}.ply") for obj_id in id2obj]
+        self._OBJID = list(id2obj.keys())
+
+    @property
+    def ID2OBJ(self):
+        return self._ID2OBJ 
+    @property
+    def NAME(self):
+        return self._NAME 
+    @property
+    def DATA_ROOT(self):
+        return self._DATA_ROOT
+    @property
+    def MODEL_PATHS(self):
+        return self._MODEL_PATHS
+    @property
+    def OBJID(self):
+        return self._OBJID
+
+if __name__ =="__main__":
+    config = DatasetConfig("tudl_bop_test", "datasets/BOP_DATASETS/tudl", {1: "dragon", 2: "frog", 3: "can"})
+    print(config.ID2OBJ)
+    print(config.NAME)
+    config.NAME= "hello"
+    print(config.NAME)
--- scripts/juan_test_scripts/render_gt.py
+++ scripts/juan_test_scripts/render_gt.py
@@ -1,29 +1,20 @@
 import os.path as osp
 from gdrn_simple.DataLoadingUtils import SimpleDataDictDataset
 from gdrn_simple.RenderClients import RendererClient
+from gdrn_simple.DatasetConfig import get_dataset_config
 from lib.pysixd.config import datasets_path
 import os
 import cv2
 
-# name = "ambf_suturing"
-# id2obj = {1: "needle"}
-# obj_ids = list(id2obj.keys())
-# model_paths = [os.path.join(datasets_path,"ambf_suturing/models/obj_000001.ply")]
-
-name = "tudl_bop_test"
-id2obj = {1: "dragon", 2: "frog", 3: "can"}
-model_paths = [osp.join(datasets_path,"tudl/models",f"obj_{obj_id:06d}.ply") for obj_id in id2obj]
-obj_ids = list(id2obj.keys())
+name = "ambf_suturing"
+# name = "tudl_bop_test"
 
 def main():
-    dataset = SimpleDataDictDataset(name) 
+    dataset_cfg = get_dataset_config(name)
+    dataset = SimpleDataDictDataset(dataset_cfg.NAME) 
     print(dataset.get_stored_keys())
     print(dataset[0]["annotations"][0].keys())
     print(len(dataset[0]["annotations"]))
-
-    for m in model_paths:
-        print(m)
-        assert osp.exists(m)
 
     rgb_im = dataset.get_rgb(0)
     pose = dataset.get_pose(0)
@@ -34,7 +25,7 @@ def main():
     print(K)
     print(f"obj_id {dataset[0]['annotations'][0]['category_id']}")
 
-    renderer = RendererClient("cpp", model_paths, obj_ids, width=640, height=480)
+    renderer = RendererClient("cpp", dataset_cfg.MODEL_PATHS, dataset_cfg.OBJID, width=640, height=480)
 
     uniform_color = [0.0, 0.5, 0.0]
     obj_id = dataset[0]["annotations"][0]["category_id"] + 1
