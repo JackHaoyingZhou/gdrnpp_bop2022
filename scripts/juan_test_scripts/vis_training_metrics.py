4b939d0822d44b1562b02b5785ce22ddf05389e8
Juan Antonio Barragan <jbarrag3@jh.edu>
2023-09-07 12:08:33 -0400

    Improve visualization scripts

--- scripts/juan_test_scripts/vis_training_metrics.py
+++ scripts/juan_test_scripts/vis_training_metrics.py
@@ -4,8 +4,9 @@ import mmcv
 import json
 import matplotlib.pyplot as plt
 
-# default_path = "./output/gdrn/tudl/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_tudl_juan/metrics.json"
-default_path = "./output/gdrn/ambf_suturing/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_ambf_suturing/metrics.json"
+default_path = "./output/gdrn/tudl/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_tudl_juan/metrics.json"
+# default_path = "./output/gdrn/ambf_suturing/convnext_a6_AugCosyAAEGray_BG05_mlL1_DMask_amodalClipBox_classAware_ambf_suturing/metrics.json"
+# default_path = "./output/gdrn/ambf_suturing/classAware_ambf_suturing_v0.0.1/metrics.json"
 # default_path = "./output/gdrn/ambf_suturing/classAware_ambf_suturing_env1_automated1/metrics.json"
 
 def on_close(event):
@@ -18,7 +19,8 @@ def on_close(event):
 def visualize_metrics(metrics_path:str):
     # metrics = mmcv.load(metrics_path)
 
-    my_metrics = ["loss_PM_R", "loss_centroid", "loss_coor_x", "loss_coor_y", "loss_coor_z", "loss_mask"]
+    my_metrics = ["loss_PM_R", "loss_centroid", "loss_coor_x", "loss_coor_y", "loss_coor_z", "loss_mask", \
+                  "loss_mask_full", "loss_region","loss_z", "total_loss"]
     metrics_parsed = defaultdict(list) 
     with open(metrics_path, "r") as f:
         for line in f:
@@ -30,11 +32,11 @@ def visualize_metrics(metrics_path:str):
             # break
             x = 0
 
-    fig, ax = plt.subplots(3,2)
+    fig, ax = plt.subplots(4,3)
     fig.canvas.mpl_connect('close_event', on_close)
-    fig.set_size_inches(12.57,5.92)
-    fig.subplots_adjust(hspace=0.38)
-
+    fig.set_size_inches(15.45,7.41)
+    fig.subplots_adjust(left=0.074, bottom=0.079, right =0.964,top=0.88,wspace=0.217, hspace=0.5536)
+    
     for m1, a1 in zip(my_metrics, ax.flatten()):
         a1.plot(metrics_parsed["iteration"], metrics_parsed[m1])
         a1.set_title(m1)
